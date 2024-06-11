081bbabd05fc2dae4f1330317827a02fba24e824
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-06-11 16:08:55 +0100

    Rewritten delete function and got it working git push origin main

--- index.js
+++ index.js
@@ -41,6 +41,63 @@ function sortArrayNoDuplicates(array) {
   return inOrder;
 }
 
+function insert(value, root) {
+  if (root === null) {
+    // If empty tree then make this the root
+    root = new Node(value);
+  } else if (value < root.data) {
+    // Recursively head down left side
+    root.left = insert(value, root.left);
+  } else if (value > root.data) {
+    // Recursively head down right side
+    root.right = insert(value, root.right);
+  } else if (value === root.data) {
+    // If it already exists within data set
+    return "This node already exists";
+  }
+  return root;
+}
+
+function deleteItem(value, tree) {
+  if (tree === null) {
+    return null;
+  }
+  // Find the node to delete
+  if (value < tree.data) {
+    tree.left = deleteItem(value, tree.left);
+  } else if (value > tree.data) {
+    tree.right = deleteItem(value, tree.right);
+  } else {
+    // Node found
+    if (tree.left === null && tree.right === null) {
+      // No children (leaf node)
+      return null;
+    } else if (tree.left === null) {
+      // One child (right)
+      return tree.right;
+    } else if (tree.right === null) {
+      // One child (left)
+      return tree.left;
+    } else {
+      // Two children
+      const nextHighest = communism(tree.right);
+      tree.data = nextHighest.data;
+      tree.right = deleteItem(nextHighest.data, tree.right);
+    }
+  }
+  return tree;
+}
+
+function communism(value) {
+  if (value.left.left !== null) {
+    communism(value.left);
+  } else {
+    const farLeft = value.left;
+    value.left = value.left.right;
+    return farLeft;
+  }
+}
+
 // Function provided by The Odin Project to help with visualisation
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -59,8 +116,21 @@ const testArray = [
   99, 17, 44, 2, 3, 7, 14, 99, 17, 56, 45, 34, 86, 56, 9, 4, 44, 17, 8, 9,
 ];
 
-console.log(sortArrayNoDuplicates(testArray)); // Testing the sort array function
+// console.log(sortArrayNoDuplicates(testArray)); // Testing the sort array function
 
 const testTree = new Tree(testArray);
 
-console.log(prettyPrint(testTree.root)); // Testing that it creates a balanced search tree from given data
+// console.log(prettyPrint(testTree.root)); // Testing that it creates a balanced search tree from given data
+
+// Testing insertion
+// console.log(insert(12, testTree.root));
+// console.log(insert(14, testTree.root));
+// console.log(insert(88, testTree.root));
+// console.log(prettyPrint(testTree.root));
+console.log(prettyPrint(testTree.root));
+
+console.log(deleteItem(99, testTree.root));
+console.log(deleteItem(2, testTree.root));
+console.log(deleteItem(45, testTree.root));
+
+console.log(prettyPrint(testTree.root));
