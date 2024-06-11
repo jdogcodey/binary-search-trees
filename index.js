67bb54fc0f5ca0b49102d5cbc56ef94e03494163
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-06-11 12:34:53 +0100

    Created Sort and remove duplicates function

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ class Node {
     this.right = null;
   }
 }
+
+// Class to store Binary Search Tree of given array
+class Tree {
+  constructor(array) {
+    this.root = buildTree(array);
+  }
+}
+
+function buildTree(array) {}
+
+function sortArrayNoDuplicates(array) {
+  const inOrder = [
+    ...new Set(
+      array.sort((a, b) => {
+        return a - b;
+      })
+    ),
+  ];
+  return inOrder;
+}
+
+const testArray = [
+  99, 17, 44, 2, 3, 7, 14, 99, 17, 56, 45, 34, 86, 56, 9, 4, 44, 17, 8, 9,
+];
+console.log(sortArrayNoDuplicates(testArray));
